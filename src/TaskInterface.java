ba3a518ed0571aa4769ff9d07fc8054e1cb6103c
beyond3rr <116442483+beyond3rr@users.noreply.github.com>
2024-10-29 12:15:43 +0500

    Update TaskInterface.java

--- src/TaskInterface.java
+++ src/TaskInterface.java
@@ -1,10 +1,11 @@
 public interface TaskInterface {
     String getTitle();
     String getDescription();
+    String getDueDate();
     boolean isCompleted();
-    void markAsCompleted();
+
     void setTitle(String title);
     void setDescription(String description);
+    void setDueDate(String dueDate);
+    void markAsCompleted();
 }
-
-
