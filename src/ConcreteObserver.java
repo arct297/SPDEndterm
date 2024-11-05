30f74adffd831dce1eb1409bda6581db2d7e9db6
beyon <beyond3r0@gmail.com>
2024-11-05 13:21:38 +0500

    NewConcrete1

---
+++ src/ConcreteObserver.java
@@ -0,0 +1,6 @@
+public class ConcreteObserver implements Observer {
+    @Override
+    public void update(String message) {
+        System.out.println("Notification received: " + message);
+    }
+}
